57caf0d1e842f5cd70cbdaffa024890f498d2e72
sout233 <2273466101@qq.com>
2024-01-24 15:47:43 +0800

    注入daisyui

--- DMWeb/DMWeb/tailwind.config.js
+++ DMWeb/DMWeb/tailwind.config.js
@@ -4,4 +4,5 @@
             './**/*.razor',//it will scan all razor files in the project and will find all the tailwind classes that you use
             './wwwroot/index.html' //not necessary if you use Blazor 8 Web App (not just standalone wasm app)
         ],
+    plugins: [require("daisyui")],
 }
