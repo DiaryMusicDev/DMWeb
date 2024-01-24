615d26b9e8e72c74476b67385988f43fe99e558d
sout233 <2273466101@qq.com>
2024-01-24 15:16:50 +0800

    tailwindcss test

--- DMWeb/DMWeb.Client/obj/Debug/net8.0/DMWeb.Client.AssemblyInfo.cs
+++ DMWeb/DMWeb.Client/obj/Debug/net8.0/DMWeb.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMWeb.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d56b173538a9ceb3c7d243d5573c114b16db32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbb79552e75c5e3574afd604758f3033c3fd013")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMWeb.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMWeb.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DMWeb/DMWeb/tailwind.config.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    content:
+        [
+            './**/*.razor',//it will scan all razor files in the project and will find all the tailwind classes that you use
+            './wwwroot/index.html' //not necessary if you use Blazor 8 Web App (not just standalone wasm app)
+        ],
+}
