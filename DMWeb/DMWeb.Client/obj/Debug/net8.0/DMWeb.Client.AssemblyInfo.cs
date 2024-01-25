eef5232de0f93d7cbbf4326919f5cf31bb78ce3b
Virtualzonemusi <130268136+diarymusic@users.noreply.github.com>
2024-01-25 10:45:00 +0800

    1

--- DMWeb/DMWeb.Client/obj/Debug/net8.0/DMWeb.Client.AssemblyInfo.cs
+++ DMWeb/DMWeb.Client/obj/Debug/net8.0/DMWeb.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMWeb.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf93991cb09bb0ba77ea2e07c79e25161d6a741")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMWeb.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMWeb.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
